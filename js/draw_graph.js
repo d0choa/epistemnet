6bbabd1097a82dc4f35ce59ee789b66a2b05f111
David Ochoa <dogcaesar@gmail.com>
2014-08-17 15:55:39 +0100

    Network draw with own data. Sticky nodes, zoom, pan, drag ok!

--- js/draw_graph.js
+++ js/draw_graph.js
@@ -1,6 +1,6 @@
     "use strict";
 
-    var NETWORK_LOCAL_DATA_URI = 'data/net2.json';
+    var NETWORK_LOCAL_DATA_URI = 'data/net.json';
     var NETWORK_WINDOW_TAG = "#network-view";
 
 	var w = window,
@@ -11,30 +11,26 @@
 		theheight = w.innerHeight || e.clientHeight|| g.clientHeight;
 
 	var n = 6;
-	var r = 5;
+	var r = 9;
     var trans=[0,0];
     var scale=1;
-	var color = d3.scale.category20();	
+	var color = d3.scale.category20c();	
 	var previousd;
 	var counter=0;
 	var centerx;
 	var centery;
-	var zoom = d3.behavior.zoom();
-	
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([0.5, 10])
+        .on("zoom", zoomed);
+		
 	var vis = d3.select(NETWORK_WINDOW_TAG)
 		.append("svg")
 		.attr("id", "playgraph")
-		// .attr({
-		// 	"width": thewidth,
-		// 	"height": theheight
-		// })
 		.attr("viewBox", "0 0 " + thewidth + " " + theheight)
 		.attr("preserveAspectRatio", "xMidYMid meet")
 		.attr("pointer-events", "all")
-		.call(zoom.on("zoom", redraw))
-		.append('svg:g')
-		// .append('svg:g')
-		 
+		.call(zoom)
+		.append('svg:g')		 
 	
 	var rect = vis.append('svg:rect')
 		.attr('width', thewidth)
@@ -43,16 +39,36 @@
 		.on("click", function(){
 			$(".pop-up").fadeOut(50);
 			previousd="";
-			d3.selectAll('[highlighted=true]').style("fill", function(d) { return color(d.GO_ref); });
+			// d3.selectAll('[highlighted=true]').style("fill", function(d) { return color(d.GO_ref); });
+			d3.selectAll('[highlighted=true]').style("fill", function(d) { return color(2); });
+			d3.selectAll('[highlighted=true]').style("stroke", function(d) { return color(); });
 			d3.selectAll('[highlighted=true]').attr("highlighted",false);
 		});	
 	
-	function redraw(){
-		$(".pop-up").fadeOut(50);
-		previousd="";
-		trans=d3.event.translate;
-		scale=d3.event.scale;		
-		vis.attr("transform","translate(" + [thewidth/2 + trans[0] - centerx, theheight/2 + trans[1] - centery] + ")"+" scale(" + scale + ")");
+	var force = d3.layout.force()
+        // .charge(-120)
+        // .linkDistance(30)
+	    .charge(-300)
+	    .linkDistance(100)
+	    .size([thewidth, theheight])
+	
+	var drag = force.drag()
+	    .on("dragstart", dragstart);
+	
+	var link = vis.selectAll(".link"),
+	    gnodes = vis.selectAll(".node");
+	
+	// function redraw(){
+	// 	$(".pop-up").fadeOut(50);
+	// 	previousd="";
+	// 	trans=d3.event.translate;
+	// 	scale=d3.event.scale;
+	// 	vis.attr("transform","translate(" + [thewidth/2 + trans[0] - centerx, theheight/2 + trans[1] - centery] + ")"+" scale(" + scale + ")");
+	// }
+	
+	function dragstart(d, i) {
+	    d3.select(this).classed("fixed", d.fixed = true);
+        $(".pop-up").fadeOut(50);
 	}
 	
 	function updateWindow(){
@@ -61,6 +77,10 @@
 		vis.attr("width", thewidth).attr("height", theheight);
 		rect.attr("width", thewidth).attr("height", theheight);
 	}
+	
+    function zoomed() {
+      vis.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
 	
 	$(window).on("resize", function() {updateWindow()}).trigger("resize");
 	
@@ -93,7 +113,9 @@
 	
 	function searchNodes(nodeNames){
 		// deletee previous
-		d3.selectAll('[highlighted=true]').style("fill", function(d) { return color(d.GO_ref); });
+		// d3.selectAll('[highlighted=true]').style("fill", function(d) { return color(d.GO_ref); });
+		d3.selectAll('[highlighted=true]').style("fill", function(d) { return color(2); });
+		d3.selectAll('[highlighted=true]').style("stroke", function(d) { return color(); });
 		d3.selectAll('[highlighted=true]').attr("highlighted",false);
 		
 		if(nodeNames != ''){
@@ -122,69 +144,45 @@
 	}
 	
 	d3.json(NETWORK_LOCAL_DATA_URI, function(error, graph) {
-		
-		var force = d3.layout.force()
-	        // .charge(-120)
-	        // .linkDistance(30)
-			.links(graph.links)
-			.nodes(graph.nodes)
-		    .size([thewidth, theheight])
-			
-		var link = vis.selectAll(".link")
-	        .data(graph.links)
+					
+		link = link.data(graph.links)
 	        .enter().append("svg:line")
-	      	.style("stroke-width", function(d) { return d.accumulated+1; })
+	      	// .style("stroke-width", function(d) { return d.accumulated+1; })
 	        .attr("class", "link")
 			.on("click",lover);
-		var node_drag = d3.behavior.drag()
-		    .on("dragstart", dragstart)
-		    .on("drag", dragmove)
-		    .on("dragend", dragend);
-			
-		function dragstart(d, i) {
-		    force.stop() // stops the force auto positioning before you start dragging
-	        $(".pop-up").fadeOut(50);
-		}
-	
-		function dragmove(d, i) {
-	        d.px += d3.event.dx;
-	        d.py += d3.event.dy;
-	        d.x += d3.event.dx;
-	        d.y += d3.event.dy; 
-		    tick(); // this is the key to make it work together with updating both px,py,x,y on d !
-		}
-	
-		function dragend(d, i) {
-		    // d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
-		    tick();
-		}
 		
-		var gnodes = vis.selectAll('.node')
-			.data(graph.nodes)
+		gnodes = gnodes.data(graph.nodes)
 			.enter()
 			.append('g')
-			.call(node_drag)
+			// .call(node_drag)
+			.call(drag)
 			.on("click",mover)
 			.classed('gnode', true);
 		var node = gnodes.append("circle")
 			.attr("class", "node")
 			.attr("r", r - .5)
 			// .on ("mouseout",mout)
-			.attr('main', function(d) {return d.main})
-	        .style("stroke", function(d) { return d3.rgb(color(d.GO_ref)).darker(); })
+			.attr('main', function(d) {return d.Entry})
+	        // .style("stroke", function(d) { return d3.rgb(color(d.GO_ref)).darker(); })
+	        .style("stroke", function(d) { return color(); })
 	        .style("stroke-width", 0.5)
-			.style("fill", function(d) { return color(d.GO_ref); });
+			// .style("fill", function(d) { return color(d.GO_ref); });
+			.style("fill", function(d) { return color(2); });
 		var labels = gnodes.append("text")
-	    	.attr("dy", ".3em")
+	    	.attr("dy", ".4em")
 	    	.attr("text-anchor", "middle")
-			.style("font-size","2.5px")
-			.text(function(d) { return d.main; });
+			.style("font-size","7px")
+			.text(function(d) { return d.Entry; });
 		
 	  	  node.append("title")
-	  	      .text(function(d) { return d.main; });
+	  	      .text(function(d) { return d.Entry; });
 		
-		// force.on("tick", tick);
-		// force.start();
+
+  		force
+  			.links(graph.links)
+  			.nodes(graph.nodes)
+  			.start()
+  			.on("tick", tick)
 		
 		function tick() {
 		  link.attr("x1", function(d) { return d.source.x; })
@@ -197,6 +195,7 @@
 				return "translate(" + d.x + "," + d.y + ")";
 			});			  
 		}
+		
 							    
 		function mover(d,i) {
 	        $(".pop-up").fadeOut(50);
@@ -204,23 +203,19 @@
 				previousd = d.name;
 		        $("#pop-up-node").fadeOut(100,function () {
 		            // Popup content
-		            $("#node-title").html(d.main + " (" + d.name + ")");
-		            $("#nodename").html(d.Protein_names);
+		            // $("#node-title").html(d.main + " (" + d.name + ")");
+		            // $("#nodename").html(d.Protein_names);
 		            $("#uniprot").html(d.Entry);
-		            $("#gofun").html(d.GO_ref);
-		            $("#gofundesc").html(d.GOref_description);
-					var enrichpval;
-					if(d.reference_pval == "NA"){
-						enrichpval="NA"
-					}else{
-						enrichpval=parseFloat(d.reference_pval.toPrecision(5));
-					}
-					$("#gofunpval").html(enrichpval) ;
-					
-		            // $("#pop-desc").html("M+T: text text test");
-	
-					// vis.attr("transform","translate(" + [thewidth/2 - centerx + trans[0], theheight/2 - centery + trans[1]] + ")"+" scale(" + scale + ")");
-	
+		            // $("#gofun").html(d.GO_ref);
+		            // $("#gofundesc").html(d.GOref_description);
+					// var enrichpval;
+					// if(d.reference_pval == "NA"){
+					// 	enrichpval="NA"
+					// }else{
+					// 	enrichpval=parseFloat(d.reference_pval.toPrecision(5));
+					// }
+					// $("#gofunpval").html(enrichpval) ;
+					//
 		            // Popup position
 		            var popLeft = (d.x*scale) + thewidth/2 - centerx + trans[0];//lE.cL[0] + 20;
 		            var popTop = (d.y*scale) + theheight/2 - centery + trans[1];//lE.cL[1] + 70;
@@ -239,59 +234,60 @@
 				previousd = d.name;
 		        $("#pop-up-link").fadeOut(100,function () {
 		            // Popup content
-		            $("#link-title").html("Interaction: " + d.name);
-					$("#orthologs").html(d.n);
-					$("#mt").html(Math.round(d.r).toFixed(5));			
-					var pval;
-					if(d.p_value == 0){
-						pval="< 1E-3";
-					}else{
-						pval=d.p_value;
-					}
-					$("#pMT").html(pval);
-					$("#context").html(d.context_lvl10);
-					var binary,complex,kpath,ecopath,reg;
-					if(d.binary_physical == "NA"){
-						binary="&#10008";
-					}else{
-						binary="&#10004";
-					}
-					if(d.ecocyc_complexes == "NA"){
-						complex="&#10008";
-					}else{
-						complex="&#10004";
-					}
-					if(d.kegg_pathways == "NA"){
-						kpath="&#10008";
-					}else{
-						kpath="&#10004";
-					}
-					if(d.ecocyc_pathways == "NA"){
-						ecopath="&#10008";
-					}else{
-						ecopath="&#10004";
-					}
-					if(d.ecocyc_regulation == "NA"){
-						reg="&#10008";
-					}else{
-						reg="&#10004";
-					}
-					$("#binary_physical").html(binary);
-					$("#ecocyc_complexes").html(complex);
-					$("#kegg_pathways").html(kpath);
-					$("#ecocyc_pathways").html(ecopath);
-					$("#ecocyc_regulation").html(reg);
-					
-		            // Popup position					
-		            var popLeft = (((d.source.x + d.target.x)/2)*scale)+thewidth/2-centerx+trans[0];//lE.cL[0] + 20;
-		            var popTop = (((d.source.y + d.target.y)/2)*scale)+theheight/2-centery+trans[1]+20;//lE.cL[1] + 70;
-		            $("#pop-up-link").css({"left":popLeft,"top":popTop});
-		            $("#pop-up-link").fadeIn(100);
+					// 		            $("#link-title").html("Interaction: " + d.name);
+					// $("#orthologs").html(d.n);
+					// $("#mt").html(Math.round(d.r).toFixed(5));
+					// var pval;
+					// if(d.p_value == 0){
+					// 	pval="< 1E-3";
+					// }else{
+					// 	pval=d.p_value;
+					// }
+					// $("#pMT").html(pval);
+					// $("#context").html(d.context_lvl10);
+					// var binary,complex,kpath,ecopath,reg;
+					// if(d.binary_physical == "NA"){
+					// 	binary="&#10008";
+					// }else{
+					// 	binary="&#10004";
+					// }
+					// if(d.ecocyc_complexes == "NA"){
+					// 	complex="&#10008";
+					// }else{
+					// 	complex="&#10004";
+					// }
+					// if(d.kegg_pathways == "NA"){
+					// 	kpath="&#10008";
+					// }else{
+					// 	kpath="&#10004";
+					// }
+					// if(d.ecocyc_pathways == "NA"){
+					// 	ecopath="&#10008";
+					// }else{
+					// 	ecopath="&#10004";
+					// }
+					// if(d.ecocyc_regulation == "NA"){
+					// 	reg="&#10008";
+					// }else{
+					// 	reg="&#10004";
+					// }
+					// $("#binary_physical").html(binary);
+					// $("#ecocyc_complexes").html(complex);
+					// $("#kegg_pathways").html(kpath);
+					// $("#ecocyc_pathways").html(ecopath);
+					// $("#ecocyc_regulation").html(reg);
+					//
+					// 		            // Popup position
+					// 		            var popLeft = (((d.source.x + d.target.x)/2)*scale)+thewidth/2-centerx+trans[0];//lE.cL[0] + 20;
+					// 		            var popTop = (((d.source.y + d.target.y)/2)*scale)+theheight/2-centery+trans[1]+20;//lE.cL[1] + 70;
+					// 		            $("#pop-up-link").css({"left":popLeft,"top":popTop});
+					// 		            $("#pop-up-link").fadeIn(100);
 		        });
 			}else{
 				previousd = "";
 			}
 	    }
+		
 		
 		$('.progress-bar').attr('aria-valuetransitiongoal', 100).progressbar();	  
 		
@@ -302,57 +298,51 @@
 		  // The ideal number of times scales with graph complexity.
 		  // Of course, don't run too long—you'll hang the page!
 		  
-		  vis.selectAll("line")
-		      .attr("x1", function(d) { return d.source.x; })
-		      .attr("y1", function(d) { return d.source.y; })
-		      .attr("x2", function(d) { return d.target.x; })
-		      .attr("y2", function(d) { return d.target.y; });
-    		
-			var allxs=0;
-			var counter=0;
-			var allys=0;
-			gnodes.attr("transform", function(d) { 
-				allxs = allxs + d.x;
-				allys = allys + d.y;
-				counter++;
-    		});
- 			centerx = allxs/counter;
-			centery = allys/counter;
-			
-			var minx=0;
-			var maxx=0;
-			var miny=0;
-			var maxy=0;
-			gnodes.attr("transform", function(d) { 
-				if(minx > d.x){
-					minx = d.x
-				}
-				if(maxx < d.x){
-					maxx = d.x
-				}
-				if(miny > d.y){
-					miny = d.y
-				}
-				if(maxy < d.y){
-					maxy = d.y
-				}
-    		});
-			rect.attr('width', (Math.abs(minx) + Math.abs(maxx)))
-			rect.attr('height', (Math.abs(miny) + Math.abs(maxy)))
-			rect.attr("transform","translate(" + [minx, miny] + ")"+" scale(" + scale + ")");
-			
-			force.start();
-			tick();
-			force.stop() // stops the force auto positioning before you start dragging
-		  	
-			vis.attr("transform","translate("+[thewidth/2 - centerx, theheight/2 - centery]+")");
+			// 		  vis.selectAll("line")
+			// 		      .attr("x1", function(d) { return d.source.x; })
+			// 		      .attr("y1", function(d) { return d.source.y; })
+			// 		      .attr("x2", function(d) { return d.target.x; })
+			// 		      .attr("y2", function(d) { return d.target.y; });
+			//
+			// var allxs=0;
+			// var counter=0;
+			// var allys=0;
+			// gnodes.attr("transform", function(d) {
+			// 	allxs = allxs + d.x;
+			// 	allys = allys + d.y;
+			// 	counter++;
+			//     		});
+			//  			centerx = allxs/counter;
+			// centery = allys/counter;
+			//
+			// var minx=0;
+			// var maxx=0;
+			// var miny=0;
+			// var maxy=0;
+			// gnodes.attr("transform", function(d) {
+			// 	if(minx > d.x){
+			// 		minx = d.x
+			// 	}
+			// 	if(maxx < d.x){
+			// 		maxx = d.x
+			// 	}
+			// 	if(miny > d.y){
+			// 		miny = d.y
+			// 	}
+			// 	if(maxy < d.y){
+			// 		maxy = d.y
+			// 	}
+			//     		});
+			// rect.attr('width', (Math.abs(minx) + Math.abs(maxx)))
+			// rect.attr('height', (Math.abs(miny) + Math.abs(maxy)))
+			// rect.attr("transform","translate(" + [minx, miny] + ")"+" scale(" + scale + ")");
+
+			// force.start();
+			// tick();
+			// force.stop() // stops the force auto positioning before you start dragging
+
+			// vis.attr("transform","translate("+[thewidth/2 - centerx, theheight/2 - centery]+")");
 			$("#loadingCon").fadeOut();
 			
-		  // svg.selectAll("circle")
-		  //     .data(nodes)
-		  //   .enter().append("circle")
-		  //     .attr("cx", function(d) { return d.x; })
-		  //     .attr("cy", function(d) { return d.y; })
-		  //     .attr("r", 4.5); 
 		}, 10);
 	});
